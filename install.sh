1a447035e5427e6cee10bc56daf4ad5286dc33cc
Mikael <mikael.thorman@gmail.com>
2025-06-22 16:05:16 +0200

    updated shell script

--- install.sh
+++ install.sh
@@ -24,4 +24,25 @@ prompt_install "Ollama" "ollama" "./install.sh"
 prompt_install "Nextcloud" "nextcloud" "./install.sh"
 prompt_install "nginx reverse-proxy" "nginx-reverse-proxy" "./install.sh"
 prompt_install "Home Assistant" "home-assistant" "./install.sh"
+prompt_install "Immich" "immich" "./install.sh"
+prompt_install "Portainer" "portainer" "./install.sh"
+prompt_install "Glance Dashboard" "glance-dashboard" "./install.sh"
+prompt_install "Headscale" "headscale" "./install.sh"
+prompt_install "Traccar" "traccar" "./install.sh"
+prompt_install "Vaultwarden" "vaultwarden" "./install.sh"
+prompt_install "Docmost" "docmost" "./install.sh"
+
+# Ask for Portainer Agent
+read -p "Do you want to install Portainer Agent (y/N)? " answer
+if [[ "$answer" =~ ^[Yy]$ ]]; then
+  echo "Installing Portainer Agent as a Docker container..."
+  if cd portainer/portainer-agent; then
+    ./install-portainer-agent.sh
+    cd ../..
+  else
+    echo "Failed to enter directory: portainer/portainer-agent"
+  fi
+else
+  echo "Not installing Portainer Agent"
+fi
 
